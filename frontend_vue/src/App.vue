3951689b1e54233359b08d2510a7520c2b4d201b
lion2 <10328036@stud.op.edu.ua>
2026-01-21 22:30:27 +0100

    Настройки для конфигов, а теперь и для фронту

--- frontend_vue/src/App.vue
+++ frontend_vue/src/App.vue
@@ -6,9 +6,7 @@ const store = useUserStore()
 </script>
 
 <template>
-  <!-- Header показываем ТОЛЬКО если юзер не авторизован -->
   <Header v-if="!store.token" />
 
-  <!-- сюда подставляются страницы -->
   <router-view />
 </template>
